a60dc2b6bd29af0b3c7b0304b0461a48497cd88f
Grigoriy116 <g.a.nikishin@mail.ru>
2023-03-30 14:16:09 +0400

    Cyclus -white, -for

---
+++ main15.js
@@ -0,0 +1,17 @@
+//While
+let i = 1;
+while (i < 20) {
+  console.log(i);
+  i++;
+}
+
+let k = 1;
+do {
+  console.log(k);
+  k++;
+} while (k < 20);
+
+//  for
+for (let j = 1; j < 20; j++) {
+  console.log(j);
+}
